6f786cc389b96390b26a73c1e6e9433b4f283a48
Zhichs <zizic.kovacevic.tonci@gmail.com>
2023-12-29 21:30:10 +0100

    .reduce test

--- callBackFuntions.js
+++ callBackFuntions.js
@@ -10,3 +10,13 @@ let printMsg = (func, num) => {
   // Pass in isEven as the callback function
 printMsg(isEven, 4); 
   // Prints: The number 4 is an even number: True.
+  
+
+const arrayOfNumbers = [1, 2, 3, 4];
+  /* .reduce - argument one and argument two are index 0 and 1 in the array, 
+    on the second pass the accumulator = return value*/
+const sum = arrayOfNumbers.reduce((accumulator, currentValue) => {  
+  return accumulator + currentValue;
+});
+
+console.log(sum); // 10
