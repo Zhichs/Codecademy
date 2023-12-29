c061cf47aab5e2e7baffb5aaf8dc8992b4a8ac3c
Zhichs <zizic.kovacevic.tonci@gmail.com>
2023-12-29 21:48:28 +0100

    Higher order func with 2 callback func's as passed in as arguments + a num

--- callBackFuntions.js
+++ callBackFuntions.js
@@ -11,6 +11,7 @@ let printMsg = (func, num) => {
 printMsg(isEven, 4); 
   // Prints: The number 4 is an even number: True.
   
+//////////////////////////////
 
 const arrayOfNumbers = [1, 2, 3, 4];
   /* .reduce - argument one and argument two are index 0 and 1 in the array, 
@@ -20,3 +21,22 @@ const sum = arrayOfNumbers.reduce((accumulator, currentValue) => {
 });
 
 console.log(sum); // 10
+
+//////////////////////////////////
+
+const getSquare = (n) => {
+  return n * n;
+}
+// added 2 to isEven just to make it unique
+const isEven2 = (n) => {
+  return n % 2 == 0;
+}
+
+let printFacts = (squareFunc, evenFunc, num) => {
+  const squareVal   = squareFunc(num);
+  const isSquareEven = evenFunc(squareVal);
+  console.log(`The square value of ${num} is ${squareVal}.`)
+  console.log(`The square value is an even number: ${isSquareEven}.`)
+}
+
+printFacts(getSquare, isEven2, 14);
