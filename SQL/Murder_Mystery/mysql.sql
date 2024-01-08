a97b1612ffc5610192c9737312f5dabdbc4ee74b
Zhichs <zizic.kovacevic.tonci@gmail.com>
2024-01-08 13:45:42 +0100

    Initail commit, data base and first file added

---
+++ SQL/Murder_Mystery/mysql.sql
@@ -0,0 +1,5 @@
+-- CREATE TABLE testers(tester_id INTEGER NOT NULL, tester_name TEXT NOT NULL, email TEXT NOT NULL);
+
+-- INSERT INTO testers VALUES(2, "Tena", "test2@gmail.com");
+
+SELECT * FROM testers;
